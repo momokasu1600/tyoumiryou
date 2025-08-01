ef452d81e09140e8577d516be03b85138d37ee46
wata28-skd <megu19730528@icloud.com>
2025-08-02 04:45:35 +0900

    wanna be a neet

--- control.gd
+++ control.gd
@@ -1,11 +1,23 @@
 extends Control
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://node_3d.tscn")
+func _ready():
+	# 外部コントローラー（Pico W）のボタン入力を接続
+	PicoWController.button_pressed.connect(_on_controller_button_pressed)
+	print("Start screen ready. Waiting for input...")
 
+# 外部コントローラーのボタンが押された時の処理
+func _on_controller_button_pressed():
+	print("Controller button pressed! Starting game...")
+	start_game()
 
+# キー入力を処理する関数（テスト用）
 func _input(event):
-	# もし押されたキーがEnterキーだったら
+	# Enterキーが押された時の処理
 	if event is InputEventKey and event.pressed and event.keycode == KEY_ENTER:
-		# ゲームシーンに切り替える
-		get_tree().change_scene_to_file("res://node_3d.tscn")
+		print("Enter key pressed! Starting game...")
+		start_game()
+
+# ゲーム開始の共通処理
+func start_game():
+	# シーン遷移
+	get_tree().change_scene_to_file("res://node_3d.tscn")
